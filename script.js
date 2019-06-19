91db0efd9104a5d3657b4f9ee7365c88b6fea090
mirsahib <mirsahib24@gmail.com>
2019-06-19 09:38:38 +0600

    mid-q4

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 /* Add your script here */
+var word = ["education", "technology", "mathematics", "science", "engineering", "arithmetic"]
+
+var guessWord = ''
+
+$(document).ready(function(){
+    $('#play').click(function(){
+        guessWord = word[Number($('#select').val())]
+        console.log(guessWord)
+        if($('#guessOutput').html()!=""){
+            $('#guessOutput').empty()
+        }
+        for(var i=0;i<guessWord.length;i++){
+            var letter = $('<h5 id="'+i+'">'+guessWord[i]+'</h5>')
+            $('#guessOutput').append(letter)
+        }
+        $('#leftside').show()
+    })
+
+
+})
